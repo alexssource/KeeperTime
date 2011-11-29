8ca02dffebb5e726d91f8ab508b138c37e75280c
Alex Kozlov <alex@alex-HP625.(none)>
2011-11-29 04:37:07 +0300

    Base directory structure

---
+++ source/main.cpp
@@ -0,0 +1,38 @@
+/*
+ * File:   main.cpp
+ * Author: alex
+ *
+ * Created on November 17, 2011, 12:15 PM
+ */
+
+/**
+ * Console Presentation
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hi, its console!" << endl;
+    
+    return 0;
+}
+
+
+/**
+ * Qt presentation
+ * 
+#include <QtGui/QApplication>
+
+int main(int argc, char *argv[]) {
+    // initialize resources, if needed
+    // Q_INIT_RESOURCE(resfile);
+
+    QApplication app(argc, argv);
+
+    // create and show your widgets here
+
+    return app.exec();
+}
+*/
